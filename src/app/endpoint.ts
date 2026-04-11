43502d8197b25d014b5842e51f038570c324f38e
Awosika Ayomide <ayomideoluwafemi2019@gmail.com>
2026-04-11 21:13:15 +0100

    feat(user):V1done

--- src/app/endpoint.ts
+++ src/app/endpoint.ts
@@ -1,6 +1,6 @@
 export const ENDPOINT = {
-//   baseUrl: 'https://tazerhstorephp.onrender.com',
-   baseUrl: 'http://localhost/tazerhstore',
+  baseUrl: 'https://tazerhstorephp.onrender.com',
+  //  baseUrl: 'http://localhost/tazerhstore',
   sellerSignup: 'sellersignup.php',
   sellerLogin: 'sellerlogin.php',
   products: 'products.php'
